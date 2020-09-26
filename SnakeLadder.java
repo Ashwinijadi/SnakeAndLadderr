4ad711d26d99fb1ce40d05bcf0bd050c23ad3c9b
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-09-26 07:02:01 +0530

    added:Snake and ladder game started at position zero

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+package snakeLadder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {		
+		//Variable
+    int dicePosition=0;
+    System.out.println("player1 is at the position:" +dicePosition);  
+  }
+}
