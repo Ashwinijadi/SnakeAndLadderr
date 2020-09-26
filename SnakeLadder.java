c0c84edcb77478ae2127692354a0207a094d08ce
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-09-26 07:13:16 +0530

    added:dice roll is added using random

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,12 @@
 package snakeLadder;
 
 public class SnakeLadder {
-	public static void main(String[] args) {		
-		//Variable
-    int dicePosition=0;
-    System.out.println("player1 is at the position:" +dicePosition);  
-  }
+	public static void main( String args[] )   
+	{
+	int min = 1;  
+	int max = 6;  
+	System.out.println("Random value of type double between "+min+" to "+max+ ":");  
+	double dice= Math.random()*(max-min+1)+min;   
+	System.out.println("dice : "+dice);  
+	} 
 }
